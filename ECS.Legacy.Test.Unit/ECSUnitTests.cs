355930722341e4afea2f458695503b8c1ab212a0
krabsen <krabsenm@gmail.com>
2018-02-15 11:08:40 +0100

    test project added

---
+++ ECS.Legacy.Test.Unit/ECSUnitTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ECS.Legacy.Test.Unit
+{
+    [TestFixture]
+    public class ECSUnitTests
+    {
+        [Test]
+        public void ctor_test()
+        {
+            var uut = new ECS(1);
+
+            var result = uut.GetThreshold();
+
+            Assert.That(result, Is.EqualTo(1));
+        }
+
+
+
+    }
+}
