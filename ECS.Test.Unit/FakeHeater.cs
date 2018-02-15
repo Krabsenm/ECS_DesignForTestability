249785a7e8c4e13b3a4015a14d1fcf71fcbf184b
Jonas Kjær Rask <jonas.krask@gmail.com>
2018-02-15 12:17:09 +0100

    Created Heater interface and fake implementation

---
+++ ECS.Test.Unit/FakeHeater.cs
@@ -0,0 +1,34 @@
+﻿namespace ECS.Test.Unit
+{
+    class FakeHeater : IHeater
+    {
+        private bool _heaterIsOn;
+        private bool _selfTest;
+
+        private bool SelfTest
+        {
+            get { return _selfTest; }
+            set { _selfTest = value; }
+        }
+
+        public bool RunSelfTest()
+        {
+            return _selfTest;
+        }
+
+        public void TurnOff()
+        {
+            _heaterIsOn = false;
+        }
+
+        public void TurnOn()
+        {
+            _heaterIsOn = true;
+        }
+
+        public bool IsOn()
+        {
+            return _heaterIsOn;
+        }
+    }
+}
